fdb5b3bb69d553aa8efe13cc260270490da8ba15
sandycpp <sandy07.yin@gmail.com>
2025-09-02 19:33:04 +0300

    Create p2.cpp

---
+++ datatahti/chapter 3 - DS and binary/p2.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+const int mxN=1e5;
+int n, m, k, a[mxN], b[mxN];
+ 
+void solve() {
+	cin >> n >> m >> k;
+	for(int i=0; i<n; i++) {
+		cin >> a[i];
+	}
+	for(int i=0; i<m; i++) {
+		cin >> b[i];
+	}
+	
+	sort(a, a+n);
+	sort(b, b+m);
+	
+	int i=0, j=0, ans=0;
+	while(i<n && j<m) {
+		if(abs(a[i]-b[j])<=k) {
+			i++, j++, ans++;
+		} else {
+			if(a[i]>b[j]) {
+				j++;
+			} else i++;
+		}
+	}
+	cout << ans << "\n";
+}
+ 
+int main() {
+	int T=1;
+	//cin >> T;
+	while(T--)
+		solve();
+	return 0;
+}
